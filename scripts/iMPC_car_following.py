00156527ac556db80ac0e811eede5b4e3d56ad4e
rongyaw <rywang5235@gmail.com>
2024-09-05 00:04:15 -0400

    Update car following iMPC construction
    Tony PC

--- scripts/iMPC_car_following.py
+++ scripts/iMPC_car_following.py
@@ -18,7 +18,7 @@ from hololens_ros_communication.msg import hololens_info
 import time
 import numpy as np
 import math
-import cvxpy as cp
+import casadi as ca
 import os
 
 from sim_env_manager import *
@@ -34,13 +34,15 @@ class iMPC_tracker():
         self.h = horizon_length
         self.dt = time_interval
         
-    def car_following_mpc(self, x_t0):
+    def car_following_mpc(self, x_t0, front_vel, front_dist):
         # Initialize state variables and system control input
-        x = cp.Variable((3, self.h + 1))
-        u = cp.Variable((1, self.h))
+        opti = ca.Opti()
+        x = opti.variable(3, self.h + 1)
+        u = opti.variable(1, self.h)
+        cost = 0.0
         
         # Add weights of MPC reference to the problem
-        P = np.diag(self.weight_list)
+        # P = np.diag(self.weight_list)
         
         # To-do: Add constraint to enforce initial state to the same as current measurement
         constr = [x[: 0] == x_t0]
@@ -55,5 +57,20 @@ class iMPC_tracker():
                       [self.dt]])
         
         for t in range(self.h):
+            # Add human driven vehicle motion model
             constr += [x[:, t + 1] == A @ x[:, t] + B @ u]
+            # Add freeway speed cost
+            cost += self.weight_list[0] * [x[1, t] - self.ref_list[0]] ** 2
+            # Add desired acceleration cost
+            cost += self.weight_list[1] * [x[2, t] - self.ref_list[1]] ** 2
+            # Add relative velocity cost
+            cost += self.weight_list[2] * [x[1, t] - front_vel[t] - self.ref_list[2]] ** 2
+            # Add distance cost
+            cost += self.weight_list[3] * [front_dist[t] - x[0, t]]
+            # Add time headway cost
+            cost += self.weight_list[4] * [[x[1, t] / [front_dist[t] - x[0, t]]] - self.ref_list[4]] ** 2
+            # Add time to collision cost
+            cost += self.weight_list[5] * [[x[1, t] - front_vel[t]] / [front_dist[t] - x[0, t]] - self.ref_list[5]] ** 2
+            # Add control cost
+            cost += self.weight_list[6] * u[0, t] ** 2
         return 0
