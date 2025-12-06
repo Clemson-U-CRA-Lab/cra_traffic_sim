e498fa869f69730539566b4748119dd6cb60d672
rongyaw <rywang5235@gmail.com>
2025-12-05 19:48:04 -0500

    Update anl sim monitor info

--- scripts/anl_sim.py
+++ scripts/anl_sim.py
@@ -185,12 +185,7 @@ if __name__ == "__main__":
         dt = time.time() - start_t - sim_t
         sim_t = time.time() - start_t
         try:
-            # print('Sim T:' + str(round(anl_sim.sim_t, 1)) + 's. Ego S: '
-            #                + str(round(anl_sim.ego_s, 2)) + 'm. Gap to front: '
-            #                + str(round(anl_sim.front_s - anl_sim.ego_s, 2)) + 'm. Ego V: '
-            #                + str(round(anl_sim.ego_v*2.237, 2)) + ' mph. Ego Acc: '
-            #                + str(round(anl_sim.acc, 3)) + ' m/s^2.', end="\r")
-            print(f"{bcolors.HEADER}Sim time: {anl_sim.sim_t:.2f}s | {bcolors.OKCYAN}Distance headway: {anl_sim.front_s - anl_sim.ego_s:.2f}m | {bcolors.OKGREEN}Ego Speed: {anl_sim.ego_v*2.237:.2f}mph  | Ego Acc: {anl_sim.acc:.3f}m/s²{bcolors.ENDC}", end="\r")
+            print(f"{bcolors.HEADER}Sim time: {anl_sim.sim_t:.2f}s | {bcolors.OKCYAN}Distance headway: {anl_sim.front_s - anl_sim.ego_s:.2f}m | {bcolors.OKCYAN}Speed gap: {anl_sim.front_v - anl_sim.ego_v:.2f}m/s | {bcolors.OKGREEN}Ego Speed: {anl_sim.ego_v*2.237:.2f}mph  | Ego Acc: {anl_sim.acc:.3f}m/s²{bcolors.ENDC}", end="\r")
             anl_sim.step_forward_kinematic_bicycle(dt=dt)
             anl_sim.pub_odom()
             rate.sleep()
