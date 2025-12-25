e0c23a6fcc3dfa1ea7a3e4342abeb34d2f42384c
rongyaw <rywang5235@gmail.com>
2025-12-25 14:27:43 -0500

    Update ttc i tracking scenario
    Tony PC Ubuntu

--- pv_scenario_generation_workspace/ttc_i_tracking_scene.py
+++ pv_scenario_generation_workspace/ttc_i_tracking_scene.py
@@ -3,50 +3,52 @@ import numpy as np
 import casadi
 import sys
 import time
+from matplotlib import pyplot as plt
 
 class preceding_vehicle_spd_profile_generation():
     def __init__(self, horizon_length, time_interval):
         self.h = horizon_length
         self.dT = time_interval
-        self.ego_a = 0.0
-        self.ego_v = 0.0
-        self.ego_s = 0.0
+        self.ego_a = np.zeros(self.h)
+        self.ego_v = np.zeros(self.h)
+        self.ego_s = np.zeros(self.h)
         
         self.pv_a = 0.0
         self.pv_v = 0.0
         self.pv_s = 0.0
         
-        self.da_opt = np.zeros(self.h)
-        self.dv_opt = np.zeros(self.h)
-        self.ds_opt = np.zeros(self.h)
+        self.pv_a_opt = np.zeros(self.h)
+        self.pv_v_opt = np.zeros(self.h)
+        self.pv_s_opt = np.zeros(self.h)
         self.ttci_opt = np.zeros(self.h)
         
         self.ttc_i = 0.0
         
     def update_ego_vehicle_state(self, ego_a_t, ego_v_t, ego_s_t, pv_a_t, pv_v_t, pv_s_t):
-        self.ego_a = ego_a_t
-        self.ego_v = ego_v_t
-        self.ego_s = ego_s_t
-        
+        # Update ego vehicle state over the horizon
+        for i in range(self.h):
+            if i == 0:
+                self.ego_a[i] = ego_a_t
+                self.ego_v[i] = ego_v_t
+                self.ego_s[i] = ego_s_t
+            else:
+                self.ego_a[i] = self.ego_a[i-1]
+                self.ego_v[i] = self.ego_v[i-1] + self.ego_a[i-1]*self.dT
+                self.ego_s[i] = self.ego_s[i-1] + self.ego_v[i-1]*self.dT + 0.5*self.ego_a[i-1]*self.dT**2
+                
+        # Update preceding vehicle state at current time   
         self.pv_a = pv_a_t
         self.pv_v = pv_v_t
         self.pv_s = pv_s_t
-        
-        self.ttc_i = (self.pv_v - self.ego_v) / (self.pv_s - self.ego_s)
     
-    def perform_nonlinear_optimization_for_pv_spd(self, ttc_i_ref, dv_tgt, dv_upper_lim, dv_lower_lim, da_upper_lim, da_lower_lim):
-        # Update preceding vehicle state
-        ds_t = self.pv_s - self.ego_s
-        dv_t = self.pv_v - self.ego_v
-        ttci_t = dv_t / ds_t
-        
+    def perform_nonlinear_optimization_for_pv_spd(self, ttc_i_ref, v_max, v_min, a_max, a_min):        
         # Create optimization problem
         opti = casadi.Opti()
         s = opti.variable(2, self.h)
         u = opti.variable(self.h - 1)
         e_spd = opti.variable(self.h)
         e_ds = opti.variable(self.h)
-        s_0 = casadi.MX(np.matrix([ds_t, dv_t]))
+        s_0 = casadi.MX(np.matrix([self.pv_s, self.pv_v]))
         
         # Align initial state
         opti.subject_to(s[:, 0] == s_0.T)
@@ -56,45 +58,43 @@ class preceding_vehicle_spd_profile_generation():
         
         # Define dynamics
         for i in range(1, self.h):
-            print(i)
             # Add ttci tracking cost
-            cost += (s[1, i] - ttc_i_ref * s[0, i])**2
+            cost += 10*((s[1, i] - self.ego_v[i]) - ttc_i_ref * (s[0, i] - self.ego_s[i]))**2
             # Add target speed tracking cost
             # cost += (s[1, i] - dv_tgt)**2
             # Add acceleration cost
-            cost += 10*u[i-1]**2
+            cost += u[i-1]**2
             # Add slack variables
             cost += 1e8*(e_spd[i]**2 + e_ds[i]**2)
             
             # Define system dynamics
             opti.subject_to(s[0, i] == s[0, i-1] + s[1, i-1]*self.dT + 0.5*u[i-1]*self.dT**2)
             opti.subject_to(s[1, i] == s[1, i-1] + u[i-1]*self.dT)
-            # opti.subject_to(s[2, i] == s[1, i] / s[0, i])
             
             # Add safe distance constraint
             opti.subject_to(s[0, i] >= 8.0 + e_ds[i])
             
             # Add speed limit constraint
-            opti.subject_to(s[1, i] <= dv_upper_lim + e_spd[i])
-            opti.subject_to(s[1, i] >= dv_lower_lim - e_spd[i])
+            opti.subject_to(s[1, i] <= v_max + e_spd[i])
+            opti.subject_to(s[1, i] >= v_min - e_spd[i])
             
             # Add acceleration limit constraint
-            opti.subject_to(u[i-1] <= da_upper_lim)
-            opti.subject_to(u[i-1] >= da_lower_lim)
+            opti.subject_to(u[i-1] <= a_max)
+            opti.subject_to(u[i-1] >= a_min)
         
         # Define minization problem
         opti.minimize(cost)
         
         # Define solver options
-        opti.solver('fatrop', {"expand": False, "print_time": 1}, {"print_level": 1})
+        opti.solver('fatrop', {"expand": True, "print_time": 0}, {"print_level": 0})
         
         sol = opti.solve()
         
         # Extract optimized values
-        self.da_opt = sol.value(u)
-        self.dv_opt = sol.value(s[1, :])
-        self.ds_opt = sol.value(s[0, :])
-        self.ttci_opt = self.dv_opt / self.ds_opt
+        self.pv_a_opt = sol.value(u)
+        self.pv_v_opt = sol.value(s[1, :])
+        self.pv_s_opt = sol.value(s[0, :])
+        self.ttci_opt = (self.pv_v_opt - self.ego_v) / (self.pv_s_opt - self.ego_s)
         
         print("Solution found!")
         
@@ -106,34 +106,51 @@ if __name__ == "__main__":
     a_max = 3.0
     a_min = -4.0
     
-    front_v = 20.0
-    front_a = 1.0
+    front_v = 4.0
+    front_a = 0.0
     front_s = 35.0
     
-    ego_v = 11.0
-    ego_a = -2.0
+    ego_v = 5.0
+    ego_a = 0.0
     ego_s = 0.0
     
-    dv = front_v - ego_v
-    da = front_a - ego_a
-    ds = front_s - ego_s
-    
-    dv_upper_lim = v_max - front_v
-    dv_lower_lim = v_min - front_v
-    
-    da_upper_lim = a_max - front_a
-    da_lower_lim = a_min - front_a
-    
-    ttc_i_ref = -0.5
-    dv_tgt = v_max - front_v
+    ttc_i_ref = -0.15
     
     pv_spd_profile_gen = preceding_vehicle_spd_profile_generation(horizon_length, time_interval)
     
     # Example usage
     pv_spd_profile_gen.update_ego_vehicle_state(ego_a, ego_v, ego_s, front_a, front_v, front_s)
-    pv_spd_profile_gen.perform_nonlinear_optimization_for_pv_spd(ttc_i_ref, dv_tgt, dv_upper_lim, dv_lower_lim, da_upper_lim, da_lower_lim)
+    t = time.time()
+    pv_spd_profile_gen.perform_nonlinear_optimization_for_pv_spd(ttc_i_ref, v_max=v_max, v_min=v_min, a_max=a_max, a_min=a_min)
+    duration = time.time() - t
+    print("Optimization duration:", duration)
     
-    print("Optimized acceleration:", pv_spd_profile_gen.da_opt)
-    print("Optimized speed:", pv_spd_profile_gen.dv_opt)
-    print("Optimized distance:", pv_spd_profile_gen.ds_opt)
-    print("Optimized TTC:", pv_spd_profile_gen.ttci_opt)
+    # Plot pv speed and ego speed and distance in two subplots
+    plt.figure()
+    plt.subplot(2, 2, 1)
+    plt.plot(pv_spd_profile_gen.pv_v_opt, label='Preceding Vehicle Speed')
+    plt.plot(pv_spd_profile_gen.ego_v, label='Ego Vehicle Speed')
+    plt.ylabel('Speed (m/s)')
+    plt.title('Preceding Vehicle and Ego Vehicle Speed Profiles')
+    plt.legend()
+    
+    plt.subplot(2, 2, 2)
+    plt.plot(pv_spd_profile_gen.pv_s_opt - pv_spd_profile_gen.ego_s, label='Distance Gap (PV - Ego)')
+    plt.ylabel('Position (m)')
+    plt.title('Preceding Vehicle and Ego Vehicle Position Profiles')
+    plt.legend()
+    
+    plt.subplot(2, 2, 3)
+    plt.plot(pv_spd_profile_gen.pv_a_opt, label='Preceding Vehicle Acceleration')
+    plt.xlabel('Horizon Step')
+    plt.ylabel('Acceleration (m/s²)')
+    plt.title('Preceding Vehicle Acceleration Profile')
+    
+    plt.subplot(2, 2, 4)
+    plt.plot(pv_spd_profile_gen.ttci_opt, label='TTCi')
+    plt.axhline(y=ttc_i_ref, color='r', linestyle='--', label='TTCi Reference')
+    plt.xlabel('Horizon Step')
+    plt.ylabel('TTCi (s)')
+    plt.title('Time-To-Collision Index (TTCi) Profile')
+    
+    plt.show()
